8221aff40054a1a54190778ad193767c18a77d0a
wintrel456 <l2T45S7E9@gmail.com>
2020-04-22 16:24:11 +0400

    Add project

---
+++ Lesson1/app/src/main/java/com/gmail/l2t45s7e9/lesson1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.l2t45s7e9.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
