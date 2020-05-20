bef660e94b1bd51fd7d91b870ef67fcbcb92778f
wintrel456 <l2T45S7E9@gmail.com>
2020-05-20 13:53:58 +0400

    Fragments

--- Lesson1/app/src/main/java/com/gmail/l2t45s7e9/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/gmail/l2t45s7e9/lesson1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.gmail.l2t45s7e9.lesson1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Frag1 frag1 = new Frag1();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.c1,frag1).commit();
+
     }
+
 }
