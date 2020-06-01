0bf1055b18d3af5ec5d9f652f1b9c03bb98003bf
wintrel456 <l2T45S7E9@gmail.com>
2020-06-01 15:41:06 +0400

    Service fix 3

--- Lesson1/app/src/main/java/com/gmail/l2t45s7e9/lesson1/ContactDetailsFragment.java
+++ Lesson1/app/src/main/java/com/gmail/l2t45s7e9/lesson1/ContactDetailsFragment.java
@@ -12,10 +12,18 @@ import androidx.fragment.app.Fragment;
 
 public class ContactDetailsFragment extends Fragment {
 
-    interface ContactDetails{
+	interface ContactDetails{
         void getContactDetails(People result);
     }
+    View view;
     ContactService contactService;
+    TextView name;
+    TextView telephoneNumber;
+    TextView telephoneNumber2;
+    TextView email;
+    TextView email2;
+    TextView description;
+
     static ContactDetailsFragment newInstance(int id) {
         ContactDetailsFragment contactDetailsFragment = new ContactDetailsFragment();
         Bundle args = new Bundle();
@@ -30,39 +38,39 @@ public class ContactDetailsFragment extends Fragment {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_contact_details, container, false);
-        ContactDetails callback = new ContactDetails() {
-            @Override
-            public void getContactDetails(People result) {
-                final People people = result;
-                final TextView name = view.findViewById(R.id.textViewName);
-                final TextView telephoneNumber = view.findViewById(R.id.textViewTelephoneNumber);
-                final TextView telephoneNumber2 = view.findViewById(R.id.textViewTelephoneNumber2);
-                final TextView email = view.findViewById(R.id.textViewEmail);
-                final TextView email2 = view.findViewById(R.id.textViewEmail2);
-                final TextView description = view.findViewById(R.id.textViewDescription);
-                if(view!=null){
-                    view.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            getActivity().setTitle(getString(R.string.ContactDetailsTitle) +": "+ people.getName());
-                            name.setText(people.getName());
-                            telephoneNumber.setText(people.getTelephoneNumber());
-                            telephoneNumber2.setText(people.getTelephoneNumber2());
-                            email.setText(people.getEmail());
-                            email2.setText(people.getEmail2());
-                            description.setText(people.getDescription());
-                        }
-                    });
-                }
-
-            }
-        };
-
+        view = inflater.inflate(R.layout.fragment_contact_details, container, false);
+        name = view.findViewById(R.id.textViewName);
+        telephoneNumber = view.findViewById(R.id.textViewTelephoneNumber);
+        telephoneNumber2 = view.findViewById(R.id.textViewTelephoneNumber2);
+        email = view.findViewById(R.id.textViewEmail);
+        email2 = view.findViewById(R.id.textViewEmail2);
+        description = view.findViewById(R.id.textViewDescription);
         int id = this.getArguments().getInt("id",0);
         contactService.getContactDetails(callback,id);
         return view;
     }
+    ContactDetails callback = new ContactDetails() {
+        @Override
+        public void getContactDetails(People result) {
+            final People people = result;
+            if(view!=null){
+                view.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(getActivity()!=null){
+                            getActivity().setTitle(getString(R.string.ContactDetailsTitle) +": "+ people.getName());
+                        }
+                        name.setText(people.getName());
+                        telephoneNumber.setText(people.getTelephoneNumber());
+                        telephoneNumber2.setText(people.getTelephoneNumber2());
+                        email.setText(people.getEmail());
+                        email2.setText(people.getEmail2());
+                        description.setText(people.getDescription());
+                    }
+                });
+            }
 
+        }
+    };
 
 }
